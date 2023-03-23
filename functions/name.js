175e44d6007e46d63cb8b76414d0eccaa4770766
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-03-23 13:46:08 -0400

    fix name function

--- functions/name.js
+++ functions/name.js
@@ -1,10 +1,6 @@
-export function onRequest(context) {
-  let name
-  name = headers.get('name');
+let name = ''
+name = headers.get('name')
 
-  if (name != '') {
-    return new Response("Hello " + {name});
+export function onRequest(context) {
+  return new Response("Hello " + name)
   }
-  else { return new Response("Hello Stranger")
-  }
- }
