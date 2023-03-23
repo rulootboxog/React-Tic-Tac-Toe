7a3493a7c91cb7ebf32bf601b66a4bbdcc3b3641
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-03-23 13:33:20 -0400

    test

--- functions/name.js
+++ functions/name.js
@@ -2,9 +2,8 @@ export function onRequest(context) {
   let name
   name = headers.get('name');
 
-  if (name != '') {
-    return new Response("Hello " + {name});
-  }
-  else { return new Response("Hello Stranger")
-  }
+  return new Response("Hello Stranger")
  }
+ 
+
+ 
