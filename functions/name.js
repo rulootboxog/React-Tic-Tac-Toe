069d5459286c16e2d43010e7f32481418046359e
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-03-23 23:18:51 -0400

    Update name.js

--- functions/name.js
+++ functions/name.js
@@ -1,6 +1,8 @@
-export function onRequest(context) {
-  let headers = new Headers();
-  var name = headers.get("name")
-  console.log(new Map(headers.get("name")));
-  return new Response("Hello " + name)
-  }
+export default {
+  async fetch(request) {
+    let headers = new Headers();
+    var name = headers.get("name")
+    console.log(new Map(headers.get("name")));
+    return new Response("Hello " + name)
+    },
+  };
