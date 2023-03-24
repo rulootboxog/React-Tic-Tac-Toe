5f5799e5a6a153370553d1be1437ff8d47149196
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-03-23 23:07:02 -0400

    Update name.js

--- functions/name.js
+++ functions/name.js
@@ -1,5 +1,6 @@
 export function onRequest(context) {
   let headers = new Headers();
   var name = headers.get("name")
+  console.log(new Map(headers.get("name")));
   return new Response("Hello " + name)
   }
