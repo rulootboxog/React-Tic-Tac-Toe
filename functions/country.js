8a8421fe8eb0f03b982f3d964844dc04f0617b95
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-03-27 16:15:42 -0400

    test

--- functions/country.js
+++ functions/country.js
@@ -1,6 +1,6 @@
 export function onRequest(context) {
   let headers = new Headers();
-  var country = headers.get("CF-IPCountry")
+  var country = headers.get("cf-ipcountry")
    console.log(new Map(headers));
   return new Response("Hello " + country + " user.")
   }
