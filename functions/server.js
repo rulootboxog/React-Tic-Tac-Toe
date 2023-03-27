aaf0a1cf942449add0155d04834f88e35102861a
brobinsoncld <80985791+rulootboxog@users.noreply.github.com>
2023-03-27 16:26:34 -0400

    update

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,7 @@
 export function onRequest(context) {
   let headers = new Headers();
-  var server = headers.get("server")
+  headers.set("server", "Cloudflare")
+  var server = headers.get("server");
    console.log(new Map(headers));
   return new Response("Hello " + server + " user.")
   }
